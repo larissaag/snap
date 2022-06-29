2dcb5820fb991b635a66c526fdfb6f7133087a58
Larissa de Almeida <larissa-a-g@hotmail.com>
2022-06-29 17:13:58 -0300

    qualquer coisa

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 let menu = document.getElementById("menu-toggle");
-let qualquerCoisa = document.querySelector("qualquer-coisa");
-menu.addEventListener("click",  ()=>{ 
-    
-} )
+let qualquerCoisa = document.getElementById("qualquer-coisa");
+menu.addEventListener("click", () => {
+    console.log(qualquerCoisa.style);
+    if (qualquerCoisa.style.right == "0px") {
+    qualquerCoisa.style.right = "-300px";
+  } else {
+    qualquerCoisa.style.right = "0";
+  }
+});
