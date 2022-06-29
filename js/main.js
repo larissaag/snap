d2c261baa6c42b94e03391f31370ab5b2e504727
Larissa de Almeida <larissa-a-g@hotmail.com>
2022-06-29 16:31:51 -0300

    responsividade

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 let menu = document.getElementById("menu-toggle");
 let qualquerCoisa = document.querySelector("qualquer-coisa");
-menu.addEventListener("click",  ()=>{
+menu.addEventListener("click",  ()=>{ 
     
 } )
